10b97034205d2b8b09eaffbfe0eb18f2f9e18806
qhdwight <qhdwight@gmail.com>
2018-03-21 22:45:45 -0700

    Added beginnings to render function and perspective function to Mat (WIP)

--- client/static/client.js
+++ client/static/client.js
@@ -1,3 +1,17 @@
+var vertexShaderSource = `
+    attribute vec4 a_VertexPosition;
+    uniform mat4 u_ModelViewProjectionMatrix;
+    void main(void) {
+        gl_Position = u_ModelViewProjectionMatrix * a_VertexPosition;
+    }
+`;
+
+const fragmentShaderSource = `
+    void main(void) {
+        gl_FragColor = vec4(0.2, 0.2, 0.2, 1.0);
+    }
+`;
+
 var cubeIndices = [
      0,  1,  2,      0,  2,  3,   // Front
      4,  5,  6,      4,  6,  7,   // Back
@@ -40,35 +54,9 @@ var cubeVertices = [
     -1.0,  1.0, -1.0,
 ];
 
-class Game {
-    constructor() {
-        this.gameCanvas = document.getElementById('gameCanvas');
-        window.addEventListener('resize', this.onResize)
-        this.onResize();
-        this.gl = this.gameCanvas.getContext('webgl');
-        if (!this.gl) {
-            console.log("Unable to initialize WebGL. Your browser or machine may not support it.");
-            return;
-        }
-        this.initGL();
-        this.initScene();
-    }
-    initGL() {
-        this.gl.clearColor(0.0, 0.0, 0.2, 1.0);
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+var gl;
 
-    }
-    initScene() {
-        var scene = new Scene();
-        var cubeMesh = new Mesh(cubeVertices, cubeIndices, []);
-        var cube = new RenderableSceneObject(this, new Vec([0, 0, 0]), new Vec([0, 0, 0]), cubeMesh);
-        scene.addSceneObject(cube)
-    }
-    onResize() {
-        gameCanvas.width = window.innerWidth;
-        gameCanvas.height = window.innerHeight;
-    }
-}
+var fov = 45 * Math.PI / 180;
 
 class Vec {
     constructor(data) {
@@ -80,18 +68,68 @@ class Mat {
     constructor(rows, columns) {
         this.rows = rows;
         this.columns = columns;
-        this.data = []
+        this.data = new Float32Array(rows * columns);
     }
     static multiply(m1, m2) {
         p = new Mat(m1.rows, m2.columns);
         for (var i = 0; i < m1.rows; i++) {
             for (var j = 0; j < m2.columns; j++) {
                 for (var k = 0; k < m1.columns; k++) {
-                    p.data[i][j] += m1.data[i][k] * m2.data[k][i];
+                    p.data[m1.rows * i + j] += m1.data[m1.rows * i + k] * m2.data[m2.rows * k + i];
                 }
             }
         }
         return p;
+    }
+    static perspective(fov, aspect, near, far) {
+        var r = new Mat(4, 4);
+        var f = 1.0 / Math.tan(fov / 2);
+        var nf = 1 / (near - far);
+        r.data[ 0] = f / aspect;
+        r.data[ 5] = f;
+        r.data[10] = (far + near) * nf;
+        r.data[11] = -1;
+        r.data[14] = 2 * far * near * nf;
+        return r;
+    }
+}
+
+class Game {
+    constructor() {
+        this.gameCanvas = document.getElementById('gameCanvas');
+        window.addEventListener('resize', this.onResize)
+        this.onResize();
+        gl = this.gameCanvas.getContext('webgl');
+        if (!gl) {
+            console.log("Unable to initialize WebGL. Your browser or machine may not support it.");
+            return;
+        }
+        // var aspect = this.gameCanvas.width / this.gameCanvas.height;
+        // var closeZ = 0.1;
+        // var farZ = 100.0;
+        // console.log(Mat.perspective(fov, aspect, closeZ, farZ).data);
+        this.initScene();
+    }
+    initScene() {
+        var scene = new Scene();
+        var cubeMesh = new Mesh(cubeVertices, cubeIndices, []);
+        var cube = new RenderableSceneObject(new Vec([0, 0, 0]), new Vec([0, 0, 0]), cubeMesh);
+        scene.addSceneObject(cube)
+    }
+    onResize() {
+        this.gameCanvas.width = window.innerWidth;
+        this.gameCanvas.height = window.innerHeight;
+    }
+    renderScene() {
+        gl.clearColor(0.0, 0.0, 0.2, 1.0);
+        gl.clearDepth(1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        var aspect = this.gameCanvas.width / this.gameCanvas.height;
+        var closeZ = 0.1;
+        var farZ = 100.0;
+        var perspectiveMatrix = Mat.perspective(fov, aspect, closeZ, farZ);
     }
 }
 
@@ -122,33 +160,32 @@ class Scene {
 }
 
 class SceneObject {
-    constructor(game) {
-        this.game = game;
+    constructor() {
     }
 }
 
 class SpacialSceneObject extends SceneObject {
-    constructor(game, pos, rot) {
-        super(game);
+    constructor(pos, rot) {
+        super();
         this.pos = pos;
         this.rot = rot;
     }
 }
 
 class RenderableSceneObject extends SpacialSceneObject {
-    constructor(game, pos, rot, mesh) {
-        super(game, pos, rot)
+    constructor(pos, rot, mesh) {
+        super(pos, rot)
         this.mesh = mesh;
         this.bindBuffers();
     }
     bindBuffers() {
-        this.vertexBuffer = this.game.gl.createBuffer();
-        this.game.gl.bindBuffer(this.game.gl.ARRAY_BUFFER, this.vertexBuffer);
-        this.game.gl.bufferData(this.game.gl.ARRAY_BUFFER, new Float32Array(this.mesh.vertices), this.game.gl.STATIC_DRAW);   
-        this.indexBuffer = this.game.gl.createBuffer();
-        this.game.gl.bindBuffer(this.game.gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-        this.game.gl.bufferData(this.game.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.mesh.indices), this.game.gl.STATIC_DRAW);
+        this.vertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.mesh.vertices), gl.STATIC_DRAW);   
+        this.indexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.mesh.indices), gl.STATIC_DRAW);
     }
 }
 
-var game = new Game();
+new Game();
